242542dea3b49120edbaf2fb877042cb7c56ddf5
Rogerfederer30 <will202b@gmail.com>
2018-08-08 15:03:57 +0800

    NEW ONE

---
+++ NEW/app/src/main/java/com/dji/mediaManagerDemo/ConnectionActivity.java
@@ -0,0 +1,485 @@
+package com.dji.mediaManagerDemo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.model.ShareVideo;
+import com.facebook.share.model.ShareVideoContent;
+import com.facebook.share.widget.ShareDialog;
+
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
+import dji.common.useraccount.UserAccountState;
+import dji.common.util.CommonCallbacks;
+import dji.keysdk.DJIKey;
+import dji.keysdk.KeyManager;
+import dji.keysdk.ProductKey;
+import dji.keysdk.callback.KeyListener;
+import dji.log.DJILog;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.products.Aircraft;
+import dji.sdk.sdkmanager.DJISDKManager;
+import dji.sdk.useraccount.UserAccountManager;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ConnectionActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private TextView mTextConnectionStatus;
+    private TextView mTextProduct;
+    //private TextView mTextModelAvailable;
+    //private TextView mVersionTv;
+
+    private Button mBtnOpen;
+    private Button mBtnRent;
+    private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
+        Manifest.permission.VIBRATE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.WAKE_LOCK,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_NETWORK_STATE,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.CHANGE_WIFI_STATE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.READ_PHONE_STATE,
+    };
+    private List<String> missingPermission = new ArrayList<>();
+    private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
+    private static final int REQUEST_PERMISSION_CODE = 12345;
+    private DJIKey firmwareKey;
+    private KeyListener firmwareVersionUpdater;
+    private boolean hasStartedFirmVersionListener = false;
+
+    public String selectedImagePath; //圖片檔案位置
+    private int SELECT_FILE = 1;
+    private Button shareButton;
+    private CallbackManager callbackManager;
+    private ShareDialog shareDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkAndRequestPermissions();
+        setContentView(R.layout.activity_connection);
+        initUI();
+
+        // Register the broadcast receiver for receiving the device connection's changes.
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DemoApplication.FLAG_CONNECTION_CHANGE);
+        registerReceiver(mReceiver, filter);
+
+        initFacebook();
+
+        shareButton = (Button) findViewById(R.id.btn_facebook);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        try {
+            SharedPreferences preferencesGet = getApplicationContext()
+                    .getSharedPreferences("image", android.content.Context.MODE_PRIVATE);
+            selectedImagePath = preferencesGet.getString("selectedImagePath",
+                    ""); // 圖片檔案位置，預設為空
+
+            Log.i("selectedImagePath", selectedImagePath + "");
+
+        } catch (Exception e) {
+        }
+
+        /*API 23以上權限申請*/
+        if (Build.VERSION.SDK_INT >= 23) {
+            int REQUEST_CODE_CONTACT = 101;
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            //驗證是否認證權限
+            for (String str : permissions) {
+                if (this.checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
+                    //申請權限
+                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
+                    return;
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if there is any missing permissions, and
+     * requests runtime permission if needed.
+     */
+    private void checkAndRequestPermissions() {
+        // Check for permissions
+        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
+            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
+                missingPermission.add(eachPermission);
+            }
+        }
+        // Request for missing permissions
+        if (!missingPermission.isEmpty() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    missingPermission.toArray(new String[missingPermission.size()]),
+                    REQUEST_PERMISSION_CODE);
+        }
+
+    }
+
+    /**
+     * Result of runtime permission request
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Check for granted permission and remove from missing list
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            for (int i = grantResults.length - 1; i >= 0; i--) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    missingPermission.remove(permissions[i]);
+                }
+            }
+        }
+        // If there is enough permission, we will start the registration
+        if (missingPermission.isEmpty()) {
+            startSDKRegistration();
+        } else {
+            showToast("Missing permissions!!!");
+        }
+    }
+
+    private void startSDKRegistration() {
+        if (isRegistrationInProgress.compareAndSet(false, true)) {
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    showToast( "registering, pls wait...");
+                    DJISDKManager.getInstance().registerApp(getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
+                        @Override
+                        public void onRegister(DJIError djiError) {
+                            if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
+                                DJILog.e("App registration", DJISDKError.REGISTRATION_SUCCESS.getDescription());
+                                DJISDKManager.getInstance().startConnectionToProduct();
+                                showToast("Register Success");
+                            } else {
+                                showToast( "Register sdk fails, check network is available");
+                            }
+                            Log.v(TAG, djiError.getDescription());
+                        }
+
+                        @Override
+                        public void onProductChange(BaseProduct oldProduct, BaseProduct newProduct) {
+                            Log.d(TAG, String.format("onProductChanged oldProduct:%s, newProduct:%s", oldProduct, newProduct));
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+        updateTitleBar();
+    }
+
+    @Override
+    public void onPause() {
+        Log.e(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.e(TAG, "onStop");
+        super.onStop();
+    }
+
+    public void onReturn(View view){
+        Log.e(TAG, "onReturn");
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        unregisterReceiver(mReceiver);
+        removeFirmwareVersionListener();
+        super.onDestroy();
+    }
+
+    private void initUI() {
+
+        mTextConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
+        //mTextModelAvailable = (TextView) findViewById(R.id.text_model_available);
+        mTextProduct = (TextView) findViewById(R.id.text_product_info);
+
+        //mVersionTv = (TextView) findViewById(R.id.textView2);
+        //.setText(getResources().getString(R.string.sdk_version, DJISDKManager.getInstance().getSDKVersion()));
+
+        mBtnOpen = (Button) findViewById(R.id.btn_open);
+        mBtnOpen.setOnClickListener(this);
+        mBtnOpen.setEnabled(false);
+        mBtnRent = (Button) findViewById(R.id.btn_rent);
+        mBtnRent.setOnClickListener(this);
+        //mBtnRent.setEnabled(false);
+
+    }
+
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            refreshSDKRelativeUI();
+        }
+    };
+
+    private void updateTitleBar() {
+        boolean ret = false;
+        BaseProduct product = DemoApplication.getProductInstance();
+        if (product != null) {
+            if(product.isConnected()) {
+                //The product is connected
+                showToast(DemoApplication.getProductInstance().getModel() + " Connected");
+                ret = true;
+            } else {
+                if(product instanceof Aircraft) {
+                    Aircraft aircraft = (Aircraft)product;
+                    if(aircraft.getRemoteController() != null && aircraft.getRemoteController().isConnected()) {
+                        // The product is not connected, but the remote controller is connected
+                        showToast("only RC Connected");
+                        ret = true;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(ConnectionActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateVersion() {
+        String version = null;
+        if (DemoApplication.getProductInstance() != null) {
+            version = DemoApplication.getProductInstance().getFirmwarePackageVersion();
+        }
+        /*
+        if (TextUtils.isEmpty(version)) {
+            mTextModelAvailable.setText("Firmware version:N/A"); //Firmware version:
+        } else {
+            mTextModelAvailable.setText("Firmware version:"+version); //"Firmware version: " +
+            removeFirmwareVersionListener();
+        }
+        */
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.btn_open: {
+                Intent intent = new Intent(this, DefaultLayoutActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.btn_rent: {
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void refreshSDKRelativeUI() {
+        BaseProduct mProduct = DemoApplication.getProductInstance();
+
+        if (null != mProduct && mProduct.isConnected()) {
+            Log.v(TAG, "refreshSDK: True");
+            mBtnOpen.setEnabled(true);
+
+            String str = mProduct instanceof Aircraft ? "DJIAircraft" : "DJIHandHeld";
+            mTextConnectionStatus.setText("Status: " + str + " connected");
+            tryUpdateFirmwareVersionWithListener();
+
+            if (null != mProduct.getModel()) {
+                mTextProduct.setText("" + mProduct.getModel().getDisplayName());
+            } else {
+                mTextProduct.setText(R.string.product_information);
+            }
+
+           // loginAccount();
+
+        } else {
+            Log.v(TAG, "refreshSDK: False");
+            mBtnOpen.setEnabled(false);
+
+            mTextProduct.setText(R.string.product_information);
+            mTextConnectionStatus.setText(R.string.connection_loose);
+        }
+    }
+
+    /*private void loginAccount(){
+        UserAccountManager.getInstance().logIntoDJIUserAccount(this,
+                new CommonCallbacks.CompletionCallbackWith<UserAccountState>() {
+                    @Override
+                    public void onSuccess(final UserAccountState userAccountState) {
+                        Log.e(TAG, "Login Success");
+                        showToast("Login Success!");
+                    }
+                    @Override
+                    public void onFailure(DJIError error) {
+                        showToast("Login Error:"
+                                + error.getDescription());
+                    }
+                });
+    }*/
+
+    private void tryUpdateFirmwareVersionWithListener() {
+        if (!hasStartedFirmVersionListener) {
+            firmwareVersionUpdater = new KeyListener() {
+                @Override
+                public void onValueChange(final Object o, final Object o1) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateVersion();
+                        }
+                    });
+                }
+            };
+            firmwareKey = ProductKey.create(ProductKey.FIRMWARE_PACKAGE_VERSION);
+            if (KeyManager.getInstance() != null) {
+                KeyManager.getInstance().addListener(firmwareKey, firmwareVersionUpdater );
+            }
+            hasStartedFirmVersionListener = true;
+        }
+        updateVersion();
+    }
+    private void removeFirmwareVersionListener() {
+        if (hasStartedFirmVersionListener) {
+            if (KeyManager.getInstance() != null) {
+                KeyManager.getInstance().removeListener(firmwareVersionUpdater);
+            }
+        }
+        hasStartedFirmVersionListener = false;
+    }
+
+    /*FaceBook*/
+    private void initFacebook() {
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+        // this part is optional
+        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+
+            @Override
+            public void onSuccess(Sharer.Result result) {
+                //分享成功的回调，在这里做一些自己的逻辑处理
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override @SuppressWarnings("deprecation")
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //facebook的界面回调
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        //選擇開啟方式
+        if (requestCode == SELECT_FILE && resultCode == RESULT_OK && null != data) {
+            Uri selectedMediaUri = data.getData();
+            if (selectedMediaUri.toString().contains("image")) {
+                String[] projection = { MediaStore.Images.Media.DATA };
+                Cursor cursor = managedQuery(selectedMediaUri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                selectedImagePath = cursor.getString(column_index);
+
+                if (ShareDialog.canShow(SharePhotoContent.class)) {
+                    Bitmap image2 = BitmapFactory.decodeFile(selectedImagePath);
+                    SharePhoto photo = new SharePhoto.Builder().setBitmap(image2).build();
+                    SharePhotoContent contentPhoto = new SharePhotoContent.Builder().addPhoto(photo).build();
+                    shareDialog.show(contentPhoto);
+                }
+            } else  if (selectedMediaUri.toString().contains("video")) {
+                String[] projection = { MediaStore.Video.Media.DATA };
+                Cursor cursor = managedQuery(selectedMediaUri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                cursor.moveToFirst();
+                selectedImagePath = cursor.getString(column_index);
+
+                if (ShareDialog.canShow(ShareVideoContent.class)) {
+                    File file = new File(selectedImagePath);
+                    Uri videoFileUri = Uri.fromFile(file);
+                    ShareVideo ShareVideo = new ShareVideo.Builder().setLocalUrl(videoFileUri).build();
+                    ShareVideoContent contentVideo = new ShareVideoContent.Builder().setVideo(ShareVideo).build();
+                    shareDialog.show(contentVideo);
+                }
+            }
+        }
+    }
+
+    private void selectImage() {
+        Intent intent1 = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent1.setType("image/* video/*");
+        startActivityForResult(Intent.createChooser(intent1, "選擇開啟圖庫"), SELECT_FILE);
+    }
+
+}
